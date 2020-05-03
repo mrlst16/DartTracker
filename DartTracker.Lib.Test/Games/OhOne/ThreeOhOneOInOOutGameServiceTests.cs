8ab7ca2dcbbe45c5ccc27fd6c6a328f87b011eeb
Matt Lantz <mattl@herbalife.com>
2020-05-02 22:04:28 -0700

    Added Tests

--- DartTracker.Lib.Test/Games/OhOne/ThreeOhOneOInOOutGameServiceTests.cs
+++ DartTracker.Lib.Test/Games/OhOne/ThreeOhOneOInOOutGameServiceTests.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using DartTracker.Lib.Games.OhOne;
+using DartTracker.Lib.Test.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,9 +13,63 @@ namespace DartTracker.Lib.Test.Games.OhOne
     public class ThreeOhOneOInOOutGameServiceTests
     {
         [TestMethod]
-        public async Task TwoPlayersWinStraightThrough()
+        public async Task TwoPlayersHit180And120()
         {
+            ThreeOhOneOinOOutGameService service
+                = new ThreeOhOneOinOOutGameService(GamesData.TwoPlayers());
 
+            await service.TakeShot(20, Model.Enum.ContactType.Triple);
+            await service.TakeShot(20, Model.Enum.ContactType.Triple);
+            await service.TakeShot(20, Model.Enum.ContactType.Triple);
+            Assert.AreEqual(121, service.Game.Players[0].Score);
+
+            await service.TakeShot(20, Model.Enum.ContactType.Double);
+            await service.TakeShot(20, Model.Enum.ContactType.Double);
+            await service.TakeShot(20, Model.Enum.ContactType.Double);
+            Assert.AreEqual(181, service.Game.Players[1].Score);
+
+        }
+
+        [TestMethod]
+        public async Task TwoPlayersGetTo21AndOneWins()
+        {
+            ThreeOhOneOinOOutGameService service
+                = new ThreeOhOneOinOOutGameService(GamesData.TwoPlayers());
+
+            await service.TakeShot(20, Model.Enum.ContactType.Triple);
+            await service.TakeShot(20, Model.Enum.ContactType.Triple);
+            await service.TakeShot(20, Model.Enum.ContactType.Triple);
+            Assert.AreEqual(121, service.Game.Players[0].Score);
+
+            await service.TakeShot(20, Model.Enum.ContactType.Triple);
+            await service.TakeShot(20, Model.Enum.ContactType.Triple);
+            await service.TakeShot(20, Model.Enum.ContactType.Triple);
+            Assert.AreEqual(121, service.Game.Players[1].Score);
+
+            await service.TakeShot(20, Model.Enum.ContactType.Triple);
+            await service.TakeShot(20, Model.Enum.ContactType.Single);
+            await service.TakeShot(20, Model.Enum.ContactType.Single);
+            Assert.AreEqual(21, service.Game.Players[0].Score);
+
+            await service.TakeShot(20, Model.Enum.ContactType.Triple);
+            await service.TakeShot(20, Model.Enum.ContactType.Single);
+            await service.TakeShot(20, Model.Enum.ContactType.Single);
+            Assert.AreEqual(21, service.Game.Players[1].Score);
+
+            //Player zero should bust
+            await service.TakeShot(20, Model.Enum.ContactType.Single);
+            Assert.AreEqual(1, service.Game.Players[0].Score);
+            await service.TakeShot(20, Model.Enum.ContactType.Single);
+            Assert.AreEqual(1, service.Game.Players[0].Score);
+
+            //Player one should win
+            await service.TakeShot(20, Model.Enum.ContactType.Single);
+            Assert.AreEqual(1, service.Game.Players[1].Score);
+            await service.TakeShot(1, Model.Enum.ContactType.Single);
+            Assert.AreEqual(0, service.Game.Players[1].Score);
+
+            Assert.IsTrue(await service.GameWon());
+            Assert.AreEqual((await service.WinningPlayer()).ID, service.Game.Players[1].ID);
         }
 
     }
